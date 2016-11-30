c6c0c9bc6dec38c22f16df3dbd49ac788672f0b8
Vibhu Yadav <vib.yad@gmail.com>
2016-11-30 16:39:55 -0500

    creating new package for string reverse

--- hello.go
+++ hello.go
@@ -1,7 +1,8 @@
 package main
 
-import "fmt"
-
+import ("fmt"
+		"github.com/vibhuyadav/stringutil"
+		)
 func main() {
-	fmt.Printf("Hello, world.\n")
+	fmt.Printf(stringutil.Reverse("!oG ,olleH")+"\n")
 }
